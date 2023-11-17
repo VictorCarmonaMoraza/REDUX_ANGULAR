7fc12c59131c0541bbe673ec76eab7f63547e679
Victor <VictorCarmona9035@hotmail.com>
2023-11-17 08:35:26 +0100

    Implementar NGRX en nuestro proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.action';
+
+
+interface AppState{
+  contador:number
+}
 
 @Component({
   selector: 'app-root',
@@ -9,14 +16,20 @@ export class AppComponent {
   title = 'redux-app';
   contador: number;
 
-  constructor() {
-    this.contador = 10;
+  constructor(private store:Store<AppState>) {
+    //this.contador = 10;
+    this.store.subscribe(state=>{
+      console.log(state);
+      this.contador= state.contador;
+    });
   }
 
   incrementar() {
-    this.contador++;
+    //this.contador++;
+    this.store.dispatch(actions.incrementar())
   }
   decrementar() {
-    this.contador--;
+    //this.contador--;
+    this.store.dispatch(actions.decrementar())
   }
 }
